6ffb81844591d0ba3582d215661b7b3f3c2ef172
Marina Goritskaia <5275747+freealise@users.noreply.github.com>
2025-01-27 21:03:27 +0300

    Update imgwarp.js

--- imgwarp.js
+++ imgwarp.js
@@ -75,7 +75,7 @@ ImgWarper.AffineDeformation.prototype.pointMover = function (point){
   for (var i = 0; i < this.n; ++i) {
     var t = this.fromPoints[i].subtract(point);
     this.w[i] = Math.pow(t.x * t.x + t.y * t.y, -this.alpha);
-    if (!this.w[i]) {alert('w '+JSON.stringify(t)+' '+this.alpha);}
+    if (this.w[i] === null) {alert('w '+JSON.stringify(t)+' '+this.alpha);}
   }
 
   var pAverage = ImgWarper.Point.weightedAverage(this.fromPoints, this.w);
